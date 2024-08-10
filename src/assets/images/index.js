cc5b648c4b824c2a07ba9605cc583d6905f461b2
Harsh Bhardwaj <bhardwaj.harsh583@gmail.com>
2024-08-10 11:27:37 +0530

    change dependency

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,8 @@
-import hero from './hero.jpg'
 import ionLogo from './ionLogo.png'
 import barcoLogo from './barcoLogo.png'
 import logo from './logo.svg'
 
 export {
-    hero,
     logo,
     barcoLogo,
     ionLogo
--- src/constants/index.js
+++ src/constants/index.js
@@ -124,8 +124,8 @@ export const projects = [
     {
         iconUrl: barcoLogo,
         theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
+        name: 'CI/CD',
+        description: '',
+        link: '',
     }
 ];
